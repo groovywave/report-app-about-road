4fab38592219257a6a6f76971c2106c59a2529fb
groovywave <vacation555@gmail.com>
2025-06-22 11:36:26 +0900

    -m "fix"

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 
 // ▼▼▼【重要】あなたのGASウェブアプリのURLに書き換えてください ▼▼▼
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxzTse4hTejMlvGFPYPpRT_4NXjAoc7TaJr_5MOZOY4eebAJotrwf-wUxrWnwseDKTMqg/exec';
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzhEPiu9Vmrr73yFB-cTU08cjd-2Dh4y-yw_0FBbHB8WqvHxrLaiVZCclEoQ-_OV6oduw/exec';
 // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
 document.addEventListener('DOMContentLoaded', function() {
