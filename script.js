1964396ea8028fd98185a23bc2dfecbc2b7f6b7c
groovywave <vacation555@gmail.com>
2025-08-11 15:20:56 +0900

    -m "fix"

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // ▼▼▼【重要】設定値を更新してください ▼▼▼
 const CONFIG = {
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzSRi5DEkGKWcdQg57j6iXtDUjL5RO3-UdTPvseSpwyvIUY1PfzgGQTSRC2eiouUFGiGg/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwemzSXF8Wu2nR__vs_5ODodHpqxm-UjcbglrkLF0zd7uVYQ2T99sKsNv8obOw37UvyCQ/exec',
   LIFF_ID: '2007739464-gVVMBAQR', // LINE Login channelのLIFF IDに変更
   MAX_RETRY_ATTEMPTS: 3,
   RETRY_DELAY: 1000,
